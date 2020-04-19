abc506b36054ee852f8253890156872a8c7597f5
phyhuhu <42459548+phyhuhu@users.noreply.github.com>
2020-04-19 16:23:53 -0500

    Delete README.mk

--- README.mk
+++
@@ -1,7 +0,0 @@
-# Gomoku Game
-
-Gomoku, also called Five in a Row, is an abstract strategy board game.
-Two players alternate turns placing a piece of their color on an empty cell. 
-The winner is the first player to form an unbroken chain of five pieces horizontally, vertically, or diagonally.
-The game is a tie if the entire board fills up without a winner. 
-The default board size is 10X10, which can be changed through "Set Up Board Size".
